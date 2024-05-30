1d89483fd24214b729636481e6abca17dfc21681
Copper Cleric <wbwbwbzheng@gmail.com>
2024-05-31 04:51:12 +0800

    add swagger to test api

--- src/routes/exerciseRoutes.ts
+++ src/routes/exerciseRoutes.ts
@@ -9,6 +9,81 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /exercises:
+ *   get:
+ *     summary: Get all exercises
+ *     responses:
+ *       200:
+ *         description: A list of exercises
+ */
+/**
+ * @swagger
+ * /exercises/{id}:
+ *   get:
+ *     summary: Get exercise by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The exercise ID
+ *     responses:
+ *       200:
+ *         description: Exercise details
+ *       404:
+ *         description: Exercise not found
+ */
+/**
+ * @swagger
+ * /exercises:
+ *   post:
+ *     summary: Create a new exercise
+ *     responses:
+ *       201:
+ *         description: Exercise created
+ */
+/**
+ * @swagger
+ * /exercises/{id}:
+ *   put:
+ *     summary: Update an exercise
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The exercise ID
+ *     responses:
+ *       200:
+ *         description: Exercise updated
+ *       404:
+ *         description: Exercise not found
+ */
+/**
+ * @swagger
+ * /exercises/{id}:
+ *   delete:
+ *     summary: Delete an exercise
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The exercise ID
+ *     responses:
+ *       200:
+ *         description: Exercise deleted
+ *       404:
+ *         description: Exercise not found
+ */
+
+
+
 router.get('/', getExercises);
 router.get('/:id', getExerciseById);
 router.post('/', createExercise);
